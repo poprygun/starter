c70e1b23bf56be788c338a4d65bba2a0e4e50ad2
Winnie Kwon <pydctw@gmail.com>
2018-06-15 12:05:47 -0700

    Add ag-Grid to frontend

--- frontend/src/views/HomeView.js
+++ frontend/src/views/HomeView.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid/dist/styles/ag-grid.css';
+import 'ag-grid/dist/styles/ag-theme-balham.css';
+
 const query = gql `{
     holidaysByCountry(countryCode: "UNITED_STATES"){
       code
@@ -16,13 +20,28 @@ class HomeView extends React.Component {
     if (data.loading) {
       return <div>Loading...</div>
     }
+    this.state = {
+        columnDefs: [
+            {headerName: "Code", field: "code"},
+            {headerName: "Description", field: "description"},
+            {headerName: "Date", field: "date"}
+
+        ],
+        rowData: data.holidaysByCountry.map((item => (
+            {code: item.code, description: item.description, date: item.holidayDate}
+        )))
+    }
     return (
-      <div>
-        {data.holidaysByCountry.map((item, index) => (
-          <p key={item.code}>
-              {item.description} {item.holidayDate}
-          </p>
-        ))}
+      <div className="ag-theme-balham"
+           style={{
+             height: '500px',
+             width: '600px' }}>
+        <AgGridReact
+          enableSorting={true}
+          enableFilter={true}
+          columnDefs={this.state.columnDefs}
+          rowData={this.state.rowData}>
+        </AgGridReact>
       </div>
     )
   }
